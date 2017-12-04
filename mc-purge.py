a44973f1c691041e9180a62205bd15b6828e30c4
rafamiga <rafamiga>
2017-12-04 21:09:21 +0100

    Włączanie/wyłączanie backendów

--- mc-purge.py
+++ mc-purge.py
@@ -15,7 +15,7 @@ from urllib import quote,quote_plus,unquote
 import urlparse
 
 # pip install python-varnish==0.2.1
-from varnish import VarnishManager as varnish_manager
+from varnish import VarnishManager, VarnishHandler
 import SocketServer
 # pip install requests
 import requests
@@ -43,7 +43,7 @@ class S(BaseHTTPRequestHandler):
         self.wfile.write('<pre>');
         if len(msg):
             self.wfile.write('<br />' + msg + '</br />');
-        self.wfile.write('<form action="/id=' + str(getid) + '" method="POST">');
+        self.wfile.write('<form action="/?id=' + str(getid) + '" method="POST">');
         self.wfile.write('<label for="sysid">system:&nbsp;</label>');
         self.wfile.write('&nbsp;');
         self.wfile.write("<select name=\"sysid\" onchange='rPage(this)'>");
@@ -60,7 +60,7 @@ class S(BaseHTTPRequestHandler):
         self.wfile.write('</select>');
 
         if (getid > 0):
-            self.wfile.write('<br />Hostname: <span class="bred">' + cfg[str(getid)]["hostname"] + '</span>');
+            self.wfile.write('<br />Hostname: <b>' + cfg[str(getid)]["hostname"] + '</b> (id=%d)' % (getid));
             self.wfile.write('<br />');
             self.wfile.write('<label for="purgeurl">ściezka lub regexp:&nbsp;</label>');
             self.wfile.write('<input name="purgeurl" type="text" value="" />');
@@ -71,25 +71,73 @@ class S(BaseHTTPRequestHandler):
                 self.wfile.write('Legenda:<br /><ul><li>podaj ścieżkę ze znakiem &quot;/&quot; na początku.</li>')
                 self.wfile.write('<li>przykład: podanie <span class="bred">/cyfryzacja/</span> wykasuje z cache wszystkie adresy rozpoczynające się od /cyfryzacja/</li>')
                 self.wfile.write('<li>przykład: <span class="bred">/.*\.css.*</span> wykasuje <u>wszystkie</u> pliki arkuszy stylu (regexp)</li>')
-                self.wfile.write('</pre></body></html>')
+                self.wfile.write('</ul>')
 
+            self.wfile.write('<hr />Status:');
+            
             for a in cfg[str(getid)]["endpoint"]:
+                vstatus = '<br />'
+
                 vmadm = str(cfg[str(getid)]["endpoint"][a]["adm"]);
                 vmsecret = cfg[str(getid)]["endpoint"][a]["secret"]
+
+                vstatus += 'Cache <b>%s</b><blockquote>' % (vmadm)
+
                 print "getid=%d vmadm=%s vmsecret=%s" % (getid, vmadm, vmsecret)
-                manager = varnish_manager((vmadm,))
+
+                vm = VarnishManager((vmadm,))
+
                 try:
-                    ok = manager.run('ping',secret=vmsecret)
-                    ok = manager.run('ban.list',secret=vmsecret)
-                    print ok
-                except:
-                    print "not ok"
+                    ok = vm.run('ping',secret=vmsecret)
+                    vstatus += '[PING OK]'
+                    print "ret=%s" % '<br />'.join(map(str, ok[0][0]))
+
+                    ok = vm.run('status',secret=vmsecret)
+                    vstatus += '<br />%s' % (''.join(map(str, ok[0][0])))
+
+                    ok = vm.run('backend.list',secret=vmsecret)
+                    bstatus = ''.join(map(str, ok[0][0]))
+                    sick = 0
+                    if re.search(' sick ',bstatus):
+                        sick = 1
                     
+                    if sick:
+                        bstatus = '<span class="bred">' + bstatus + '</span>'
+                    vstatus += bstatus
+
+                    vstatus += '<form action="/?id=' + str(getid) + '" method="POST">'
+                    vstatus += '<input type="hidden" name="sysid" value="' + str(getid) + '">'
+                    vstatus += '<input type="hidden" name="backend_'
+                    vstatus += "on" if sick else "off"
+                    vstatus += '" value="' + a + '">'
+                    vstatus += '<input type="submit" value="'
+                    vstatus += "WŁĄCZ" if sick else "wyłącz"
+                    vstatus += '"></form>'
+                    
+                    ok = vm.run('ban.list',secret=vmsecret)
+                    skipline = 0
+                    for l in ''.join(map(str, ok[0][0])).split('\n'):
+                        if re.search(r'C\s*$',l):
+                            skipline += 1
+                        else:
+                            vstatus += '<br />' + l                        
+
+                    if skipline:
+                        vstatus += '(liczba ukrytych zleceń wykonanych=%d)' % skipline
+
+                except Exception as e:
+                    print "not ok e=%s" % e
+                    vstatus += '<br /><span class="bred">NASTĄPIŁ BŁAD "%s"</span>' % e
+                
+                self.wfile.write(vstatus + "</blockquote>");
+
+            self.wfile.write('</pre></body></html>')
 
     def get_getid(self):
         global getid
         
         getid = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('id', None)
+
         try:
             getid = int(getid[0])
         except:
@@ -132,18 +180,57 @@ class S(BaseHTTPRequestHandler):
 
     def do_POST(self):
         global msg
+        global getid
         
-        msg = ""
-        purgeurl = ""
-        sysid = ""
-
+        msg = ''
+        purgeurl = ''
+        sysid = ''
+        off = ''
+        backend_mng = ()
+        
         postvars = self.parse_POST()
         # print str(postvars)
 
-        if "purgeurl" in postvars:
-            purgeurl = postvars['purgeurl'][0]
         if "sysid" in postvars:
             sysid = postvars['sysid'][0]
+        if "purgeurl" in postvars:
+            purgeurl = postvars['purgeurl'][0]
+
+        msg = msg + "<b>[" + cfg[sysid]["alias"] + "]</b><br />"
+
+        if "backend_off" in postvars:
+            backend_mng = (postvars['backend_off'][0],False)
+
+        if "backend_on" in postvars:
+            backend_mng = (postvars['backend_on'][0],True)
+
+        # management backendu
+	if len(backend_mng) == 2:
+            print "*** backend='%s' akcja='%s' sysid='%s'" % (backend_mng[0],str(backend_mng[1]),sysid)
+            
+            for a in cfg[sysid]["endpoint"]:
+                if backend_mng[0] == a:
+                    msg += 'Zlecenie <b>' + ('włączenia' if backend_mng[1] else 'WYŁĄCZENIA') + '</b> serwera cache: %s<br />' % a
+
+                    vmadm = str(cfg[str(getid)]["endpoint"][a]["adm"]);
+                    vmsecret = cfg[str(getid)]["endpoint"][a]["secret"]
+                    msg += '>>>%s' % (vmadm)
+                    vm = VarnishManager((vmadm,))
+
+                    try:
+                        ok = vm.run('backend.set.health','boot.default1','probe' if backend_mng[1] else 'sick',secret=vmsecret)
+                        msg += '<br />%s' % (''.join(map(str,ok[0][0]))) + '<br />'
+                        
+                    except Exception as e:
+                        print "not ok e=%s" % e
+                        msg += '<br /><span class="bred">NASTĄPIŁ BŁAD "%s"</span>' % e
+                    
+        # nic więcej nie robimy, uniwersalny koniec metod innych niż purgeurl
+        if len(purgeurl) < 1:
+            self._display_form()
+            return True
+    
+        ### reszta to PURGE/BAN ###
 
         print "*** purgeurl='%s' sysid='%s'" % (purgeurl,sysid)
 
@@ -156,19 +243,29 @@ class S(BaseHTTPRequestHandler):
                 msg = "BŁĄD: Ścieżka musi zaczynać się od znaku &quot;/&quot;<br />"
                 break
 
-            msg = msg + "<b>[" + cfg[sysid]["alias"] + "]</b><br />"
-    
             for a in cfg[sysid]["endpoint"]:
-                vm = varnish.VarnishManager(cfg[sysid]["endpoint"][a]["adm"])
-                print vm
-                
+
+#                vmadm = str(cfg[sysid]["endpoint"][a]["adm"]);
+#                vmsecret = cfg[sysid]["endpoint"][a]["secret"]
+#                vmadm = VarnishManager((vmadm,))
+
+#                try:
+#                    ok = vmadm.run('ban','req.http.host == %s && req.url ~ %s' % (cfg[sysid]["hostname"],purgeurl),secret=vmsecret)
+#                    msg = msg + '<br />%s' % (''.join(map(str,ok[0][0])))
+#                except Exception as e:
+#                    msg += '<br /><span class="bred">NASTĄPIŁ BŁAD "%s"</span>' % e
+#                    raise
+
+#            break
+
                 url = "http://" + cfg[sysid]["endpoint"][a]["host"] + ":" + cfg[sysid]["endpoint"][a]["port"] + purgeurl
                 hostname = cfg[sysid]["hostname"]
                 msg = msg + "* " + url + " Host: " + hostname + " ...<br />&nbsp;&nbsp;"
 
                 try:    
                     hdrs = { "Host": hostname }
-                    resp = requests.request("PURGE", url, timeout=2, headers=hdrs)
+#                    resp = requests.request("PURGE", url, timeout=2, headers=hdrs)
+                    resp = requests.request("XBAN", url, timeout=2, headers=hdrs)
 
                     if resp.status_code > 299:
                         msg = msg + "<b>[NIEPOWODZENIE]</b> "
@@ -194,11 +291,13 @@ class S(BaseHTTPRequestHandler):
             break    
 
         self._display_form()
+        return True
         
 def run(server_class=HTTPServer, handler_class=S, port=3000):
     global msg
     msg = ""
 
+    dir(VarnishManager)
 #    for sysid in cfg:
 #        for e in cfg[sysid]["endpoint"]:
 #            print 'adm='+cfg[sysid]["endpoint"][e]["adm"]
